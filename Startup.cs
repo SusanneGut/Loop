bb1075d8cbb20d899ee55e641b8b7793970ac131
Susanne <gutsusanne@gmail.com>
2018-12-18 13:55:02 +0100

    jag vill ha!

--- Startup.cs
+++ Startup.cs
@@ -23,6 +23,7 @@ namespace Loop
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
+			app.UseDeveloperExceptionPage();
 			app.UseStaticFiles();
 			app.UseMvcWithDefaultRoute();
 
