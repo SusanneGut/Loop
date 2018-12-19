5a6070679f26323dbf39333c7f392987c9dbc474
Thenander <info@microlab.se>
2018-12-19 14:49:20 +0100

    Mysko

--- Startup.cs
+++ Startup.cs
@@ -14,39 +14,39 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Loop
 {
-	public class Startup
-	{
-		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-		public void ConfigureServices(IServiceCollection services)
-		{
-			var connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LoopDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-			services.AddDbContext<IdentityDbContext>(o => o.UseSqlServer(connString));
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LoopDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            services.AddDbContext<IdentityDbContext>(o => o.UseSqlServer(connString));
 
-			services.AddIdentity<IdentityUser, IdentityRole>(o =>
-			{
-				o.Password.RequireNonAlphanumeric = false;
-				o.Password.RequiredLength = 6;
-			})
-			.AddEntityFrameworkStores<IdentityDbContext>()
-			.AddDefaultTokenProviders();
+            services.AddIdentity<IdentityUser, IdentityRole>(o =>
+            {
+                o.Password.RequireNonAlphanumeric = false;
+                o.Password.RequiredLength = 6;
+            })
+            .AddEntityFrameworkStores<IdentityDbContext>()
+            .AddDefaultTokenProviders();
 
-			// Only needed if login path shoudn't be "/Account/Login" 
-			services.ConfigureApplicationCookie(o => o.LoginPath = "/LogIn");
+            // Only needed if login path shoudn't be "/Account/Login" 
+            services.ConfigureApplicationCookie(o => o.LoginPath = "/LogIn");
 
-			services.AddTransient<GuestsService>();
-			services.AddMvc();
-		}
+            services.AddTransient<GuestsService>();
+            services.AddMvc();
+        }
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-		{
-			app.UseAuthentication();
-			app.UseDeveloperExceptionPage();
-			app.UseStaticFiles();
-			app.UseMvcWithDefaultRoute();
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseAuthentication();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
 
 
-		}
-	}
+        }
+    }
 }
