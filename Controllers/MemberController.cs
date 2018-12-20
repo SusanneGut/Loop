faed193e0aa11be41756940564f59d43b4c1952e
Thenander <info@microlab.se>
2018-12-20 14:19:36 +0100

    Skapat Create Activity, vilken i dagsläget INTE sparar i databasen. Vet ej varför.

--- Controllers/MemberController.cs
+++ Controllers/MemberController.cs
@@ -12,19 +12,37 @@ namespace Loop.Controllers
     [Authorize]
     public class MemberController : Controller
     {
-		MembersService service;
-		public MemberController(MembersService service)
-		{
-			this.service = service;
-		}
+        MembersService service;
+        public MemberController(MembersService service)
+        {
+            this.service = service;
+        }
 
         [HttpGet]
         [Route("Member")]
         public IActionResult Index()
         {
+            if(!ModelState.IsValid)
+            {
+                return View(nameof(Index));
+            }
             return View(new MemberIndexVM { Username = User.Identity.Name });
         }
 
-		
-	}
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(MemberCreateVM activity)
+        {
+            service.AddActivity(activity);
+            return Content("Check if activity is saved in Database");
+            //return View(nameof(Index));
+        }
+
+    }
 }
