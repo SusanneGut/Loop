3765238ce63f3dfbc731844cbcde3b02a0d164e9
Susanne <gutsusanne@gmail.com>
2019-01-07 09:44:23 +0100

    commit

---
+++ WhiteSpaceAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Loop
+{
+	public class WhiteSpaceAttribute: ValidationAttribute
+	{
+		Regex regex = new Regex(@"^\s");
+
+		public override bool IsValid(object value)
+		{
+			return regex.IsMatch(value.ToString());
+
+		}
+	}
+}
