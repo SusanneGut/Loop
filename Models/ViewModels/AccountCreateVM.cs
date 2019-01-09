4b5142213cdc8d7bd7963170582f5d578a60fcf9
Thenander <info@microlab.se>
2019-01-09 09:19:58 +0100

    Less ändringar.

--- Models/ViewModels/AccountCreateVM.cs
+++ Models/ViewModels/AccountCreateVM.cs
@@ -13,7 +13,7 @@ namespace Loop.Models.ViewModels
 
         [Display(Prompt = "Username")]
         [Required(ErrorMessage = "Enter username")]
-	public string UserName { get; set; }
+	    public string UserName { get; set; }
 
         [Display(Prompt = "E-mail")]
         [Required(ErrorMessage = "Enter E-mail")]
