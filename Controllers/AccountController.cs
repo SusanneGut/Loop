ade863c4988404f7b74e901c27750de4424023f9
Susanne <gutsusanne@gmail.com>
2019-01-10 11:03:05 +0100

    rensning och lite annat

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -9,61 +9,55 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Loop.Controllers
 {
-    public class AccountController : Controller
-    {
-        AccountService service;
+	public class AccountController : Controller
+	{
+		AccountService service;
 
 		public AccountController(AccountService service)
-        {
-            this.service = service;
-        }
-
-		[HttpGet]
-		public IActionResult Details()
 		{
-			if (!ModelState.IsValid)
-			{
-				return View(nameof(Login));
-			}
-			return View(new AccountDetailsVM { UserName = User.Identity.Name });
+			this.service = service;
 		}
 
-        [HttpGet]
-        [Route("")]
-        [Route("login")]
-        public IActionResult Login(string returnUrl)
-        {
-            var model = new AccountLoginVM
-            {
-                ReturnUrl = returnUrl
-            };
-            return View(model);
-        }
+		[HttpGet]
+		[Route("")]
+		[Route("login")]
+		public IActionResult Login(string returnUrl)
+		{
+			if (User.Identity.IsAuthenticated)
+			{
+				return RedirectToAction(nameof(MemberController.Activities), "member");
+			}
+			var model = new AccountLoginVM
+			{
+				ReturnUrl = returnUrl
+			};
+			return View(model);
+		}
 
-        [HttpPost]
-        [Route("login")]
-        public async Task<IActionResult> Login(AccountLoginVM viewModel)
-        {
-            if(!ModelState.IsValid)
-                return View(viewModel);
+		[HttpPost]
+		[Route("login")]
+		public async Task<IActionResult> Login(AccountLoginVM viewModel)
+		{
+			if (!ModelState.IsValid)
+				return View(viewModel);
 
 			if (!await service.TryLoginAsync(viewModel))
-            {
-                ModelState.AddModelError(nameof(AccountLoginVM.UserName), "Wrong username or password");
-                return View(viewModel);
-            }
+			{
+				ModelState.AddModelError(nameof(AccountLoginVM.UserName), "Wrong username or password");
+				return View(viewModel);
+			}
 
-            if(string.IsNullOrWhiteSpace(viewModel.ReturnUrl))
-                return RedirectToAction(nameof(MemberController.Activities), "member");
-            else
-                return Redirect(viewModel.ReturnUrl);
-        }
+			if (string.IsNullOrWhiteSpace(viewModel.ReturnUrl))
+				return RedirectToAction(nameof(MemberController.Activities), "member");
+			else
+				return Redirect(viewModel.ReturnUrl);
+		}
 
-        [HttpGet]
-        public IActionResult Create()
-        {
-            return View();
-        }
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
 
 		[HttpPost]
 		public async Task<IActionResult> Create(AccountCreateVM viewModel)
@@ -76,16 +70,19 @@ namespace Loop.Controllers
 				ModelState.AddModelError(nameof(AccountCreateVM.UserName), "Oups, something went wrong, try another username!");
 				return View(viewModel);
 			}
-			
+
 			return RedirectToAction(nameof(MemberController.Activities), "member");
 
 		}
 
-		[HttpPost]
-		public async Task<IActionResult> Logout()
+		[HttpGet]
+		public IActionResult Details()
 		{
-			await service.LogOut();
-			return RedirectToAction(nameof(Login));
+			if (!ModelState.IsValid)
+			{
+				return View(nameof(Login));
+			}
+			return View(new AccountDetailsVM { UserName = User.Identity.Name });
 		}
 
 		[HttpGet]
@@ -101,7 +98,15 @@ namespace Loop.Controllers
 		{
 			await service.EditAsync(User);
 
-			return RedirectToAction(nameof(MemberController.Activities),"member");
+			return RedirectToAction(nameof(MemberController.Activities), "member");
 		}
+
+		[HttpPost]
+		public async Task<IActionResult> Logout()
+		{
+			await service.LogOut();
+			return RedirectToAction(nameof(Login));
+		}
+
 	}
 };
--- Models/ViewModels/AccountUserVM.cs
+++ Models/ViewModels/AccountUserVM.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
 
-namespace Loop.Models.ViewModels
-{
-	public class AccountUserVM
-	{
-		public string UserName { get; set; }
-	}
-}
+//namespace Loop.Models.ViewModels
+//{
+//	public class AccountUserVM
+//	{
+//		public string UserName { get; set; }
+//	}
+//}
